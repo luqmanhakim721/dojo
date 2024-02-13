4b599a8f22222cdafe74b9d1c0cb5f2f7930a3d5
luqmanhakim721 <luqmanfajrin@gmail.com>
2024-02-13 04:02:06 +0000

    Commit 1707796926

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+module.exports = {
+siteUrl: 'https://dojo-gamma-tawny.vercel.app',
+generateRobotsTxt: true,
+sitemapSize: 7000,
+}
--- theme.config.js
+++ theme.config.js
@@ -2,7 +2,7 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
+    <div><small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Your Name.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
@@ -16,6 +16,8 @@ export default {
           }
         }
       `}</style>
-    </small>
+    </small><div class="stats">    <script type="text/javascript" src="https://cdn.statically.io/gh/luqmanhakim721/js/main/social.js"></script>
+<script type="text/javascript" src="https://cdn.statically.io/gh/luqmanhakim721/js/main/footer.js"></script>
+<script type="text/javascript" src="https://cdn.statically.io/gh/luqmanhakim721/js/main/stats2.js"></script></div></div>
   )
 }
